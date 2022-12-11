6640f1948ac378299c5ca708886be9f8375c3d2e
Kiril <kiril_s@yahoo.com>
2022-12-11 17:06:57 +0100

    add second component WIP

--- components/SecondComponent.tsx
+++ components/SecondComponent.tsx
@@ -1,24 +1,65 @@
 function SecondComponent() {
   return (
     <div
-      className="flex flex-col justify-center text-center items-center bg-[#eef0fc]"
+      className="flex flex-col justify-center text-center items-center text-[#1c2349] bg-white "
       id="überMich"
     >
-      <div className="flex flex-col text-[#5c6da5]  justify-start text-left items-start pt-[4rem] pl-[20%] pr-[20%]">
-        <div className="italic text-[32px]">Über Mich</div>
-        <div className="pb-[4rem]">Mein Werdegang</div>
-        <p className="text-[21px] text-[#1c2349]">
-          Meine Leidenschaft für Wirtschaft, soziale Medien und Marketing und
-          meine beruflichen Erfahrungen haben mich für die Welt des Marketings
-          ideal ausgerüstet. Als motivierte Teamplayerin mit sehr guten
-          Kommunikationsfähigkeiten, freue ich mich auf Herausforderungen. Eine
-          Beschreibung meiner Fähigkeiten finden Sie hier auf meiner Website.
-          Meine Leidenschaft für Wirtschaft, soziale Medien und Marketing und
-          meine beruflichen Erfahrungen haben mich für die Welt des Marketings
-          ideal ausgerüstet. Als motivierte Teamplayerin mit sehr guten
-          Kommunikationsfähigkeiten, freue ich mich auf Herausforderungen. Eine
-          Beschreibung meiner Fähigkeiten finden Sie hier auf meiner Website.
-        </p>
+      <div className="flex flex-col   justify-start text-left items-start  ">
+        <div className="bg-[#eef0fc] pt-[4rem] pl-[20%] pr-[20%]">
+          <div className="italic text-[32px] text-[#5c6da5]">Über Mich</div>
+          <div className="pb-[3rem] text-[#5c6da5]">Mein Werdegang</div>
+          <p className="text-[21px] pb-[4rem] ">
+            Meine Leidenschaft für Wirtschaft, soziale Medien und Marketing und
+            meine beruflichen Erfahrungen haben mich für die Welt des Marketings
+            ideal ausgerüstet. Als motivierte Teamplayerin mit sehr guten
+            Kommunikationsfähigkeiten, freue ich mich auf Herausforderungen.
+            Eine Beschreibung meiner Fähigkeiten finden Sie hier auf meiner
+            Website. Meine Leidenschaft für Wirtschaft, soziale Medien und
+            Marketing und meine beruflichen Erfahrungen haben mich für die Welt
+            des Marketings ideal ausgerüstet. Als motivierte Teamplayerin mit
+            sehr guten Kommunikationsfähigkeiten, freue ich mich auf
+            Herausforderungen. Eine Beschreibung meiner Fähigkeiten finden Sie
+            hier auf meiner Website.
+          </p>
+        </div>
+        <div className="flex pt-[4rem] pl-[20%] pr-[20%]">
+          <div className="flex-col pr-[10rem]">
+            <div className="italic text-[32px] w-[50%] text-[#5c6da5]">
+              Ausbildung
+            </div>
+            <div className="pb-[3rem] text-[#5c6da5]">Was ich gelernt habe</div>
+            <div>2018-2022</div>
+            <div>Hochschule RheinMain, Jahrgang 2018</div>
+            <div className="pb-[3rem]">
+              Bachelor of Science; Angewandte Informatik
+            </div>
+            <div>2016-2018</div>
+            <div>TU Darmstadt, Jahrgang 2016</div>
+            <div>Bachelor of Science; Informatik</div>
+            <div className="pb-[3rem]">
+              Fortsetzung in 2018 an der Hochschule RheinMain
+            </div>
+            <div>2007-2014</div>
+            <div>Universität Goce Delcev Shtip, Mazedonien, Jahrgang 2007</div>
+            <div className="pb-[3rem]">
+              Bachelor of Science; Informatik (6 Semester)
+            </div>
+          </div>
+          <div className="flex-col">
+            <div className="italic text-[32px] w-[50%] text-[#5c6da5]">
+              Arbeitserfahrung
+            </div>
+            <div className="pb-[3rem] text-[#5c6da5]">
+              Wo ich gearbeitet habe
+            </div>
+            <div>März 2022-Dezember 2022</div>
+            <div>Daubit Programmierung Service GmbH</div>
+            <div className="pb-[3rem]">Praktikant (Webentwicklung)</div>
+            <div>2018-2019</div>
+            <div>Henkel & Co. Sektkellerei</div>
+            <div className="pb-[3rem]">Werkstudent (Umwelt & Sicherheit)</div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -3,17 +3,17 @@ function Header() {
   return (
     <div className=" flex sticky w-[100%]  justify-center items-center text-center  bg-[#586cdc] font-bold italic text-white">
       <Link href="/#überMich">
-        <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer">
+        <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer duration-700">
           Über Mich
         </div>
       </Link>
-      <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer">
+      <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer duration-700">
         Ausbildung & Berufserfahrung
       </div>
-      <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer">
+      <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer duration-700">
         Fähigkeiten & Sprachen
       </div>
-      <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer">
+      <div className="py-5 mx-5 hover:text-[#5c6da5] cursor-pointer duration-700">
         Auszeichungen & Interessen
       </div>
     </div>
